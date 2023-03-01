121a76218619e2888aabc9dc18c4b79cc99572ce
CastroJ12 <chepe.castro2312@gmail.com>
2023-03-01 15:18:19 -0800

    Added StudentController, JunitTestStudent for Assignment 2

--- cst438_register/src/test/java/com/cst438/JunitTestSchedule.java
+++ cst438_register/src/test/java/com/cst438/JunitTestSchedule.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
+import com.cst438.domain.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,13 +25,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.cst438.controller.ScheduleController;
-import com.cst438.domain.Course;
-import com.cst438.domain.CourseRepository;
-import com.cst438.domain.Enrollment;
-import com.cst438.domain.EnrollmentRepository;
-import com.cst438.domain.ScheduleDTO;
-import com.cst438.domain.Student;
-import com.cst438.domain.StudentRepository;
 import com.cst438.service.GradebookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -56,6 +50,7 @@ public class JunitTestSchedule {
 
 	static final String URL = "http://localhost:8080";
 	public static final int TEST_COURSE_ID = 40442;
+	public static final int TEST_STUDENT_ID = 1;
 	public static final String TEST_STUDENT_EMAIL = "test@csumb.edu";
 	public static final String TEST_STUDENT_NAME  = "test";
 	public static final int TEST_YEAR = 2021;
@@ -211,7 +206,7 @@ public class JunitTestSchedule {
 		// verify that repository delete method was called.
 		verify(enrollmentRepository).delete(any(Enrollment.class));
 	}
-		
+
 	private static String asJsonString(final Object obj) {
 		try {
 
